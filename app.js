ee3792ba8cc849e5e0e823647b6aa02227c63f12
Matt Chastain <mattchastain@food.attlocal.net>
2015-03-09 18:21:36 -0400

    Submit Added

--- app.js
+++ app.js
@@ -15,43 +15,42 @@ angular.module('Logic').controller('GetLogicOut', function() {
 			huge_number: "",
 		}
 
+		self.displayLib = function () {
+			self.libShow = true;
+		}
+
 		self.questions = false;
 
 		self.female = true;
 
+		self.libShow = false;
+
+		self.sex = true;
+
 		self.respond = function(stuff) {
 			self.questions = true;
 			self.female = stuff;
+			self.sex = false;
 		}
 
-		// self.conditions = function() {
-		// 	if (!self.inputs.name) {
-		// 		self.inputs.name = "__________";
-		// 	}
-		// 	if (!self.inputs.job_title) {
-		// 		self.inputs.job_title = "__________";
-		// 	}
-		// 	if (!self.inputs.tedious_task) {
-		// 		self.inputs.tedious_task = "__________";
-		// 	}
-		// 	if (!self.inputs.dirty_task) {
-		// 		self.inputs.dirty_task = "__________";
-		// 	}
-		// 	if (!self.inputs.celebrity) {
-		// 		self.inputs.celebrity = "__________";
-		// 	}
-		// 	if (!self.inputs.useless_skill) {
-		// 		self.inputs.useless_skill = "__________";
-		// 	}
-		// 	if (!self.inputs.adjective) {
-		// 		self.inputs.adjective = "__________";
-		// 	}
-		// 	if (!self.inputs.obnoxious_celebrity) {
-		// 		self.inputs.obnoxious_celebrity = "__________";
-		// 	}
-		// 	if (!self.inputs.huge_number) {
-		// 		self.inputs.huge_number = "__________";
-		// 	}
-		// 	console.log(self.inputs.name);
-		// }
+		self.displayLib = function() {
+			self.libShow = true;
+			self.questions = false;
+		}
+
+		self.startOver = function() {
+			self.sex = true;
+			self.libShow = false;
+			self.inputs = {
+				name: "",
+				job_title: "",
+				tedious_task: "",
+				dirty_task: "",
+				celebrity: "",
+				useless_skill: "",
+				adjective: "",
+				obnoxious_celebrity: "",
+				huge_number: "",
+			}
+		}
 	})
